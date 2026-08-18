6f47cecd2ab58dfef897c37800c4ac42d9090d8c
Abidski <abijanahmed@gmail.com>
2026-08-18 18:04:42 -0400

    Remove sidebar

--- app/classes/layout.tsx
+++ app/classes/layout.tsx
@@ -7,11 +7,13 @@ export default async function AppLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  /*
   const supabase = await createClient();
   const { data } = await supabase.from("classes").select("*");
+  */
   return (
     <SidebarProvider>
-      <AppSidebar classes={data} />
+      {/*<AppSidebar classes={data} />*/}
       <SidebarTrigger />
       <main className="flex-1 overflow-hidden p-8">{children}</main>
     </SidebarProvider>
